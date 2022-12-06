d1a673cc897eb51972bb9b8334036c00c0769bf4
sugawara kensuke <superradio86@gmail.com>
2022-12-06 23:46:48 +0900

    fix test for session.

--- auth/app/server/id.go
+++ auth/app/server/id.go
@@ -27,3 +27,17 @@ func CreateCode() (string, error) {
 	}
 	return string(buf), nil
 }
+
+func CreateCodeChallenge() (string, error) {
+	letters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	buf := make([]rune, 45)
+	for i := range buf {
+		len := int64(len(letters))
+		num, err := rand.Int(rand.Reader, big.NewInt(len))
+		if err != nil {
+			return "", err
+		}
+		buf[i] = letters[int(num.Int64())]
+	}
+	return string(buf), nil
+}
--- auth/app/server/session_test.go
+++ auth/app/server/session_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestNewSession(t *testing.T) {
 	s := NewSession("session_id")
 	s.Set("key1", "value1")
-	actual := s.Get("key1")
+	actual := s.Get("key1").(string)
 	expected := "value1"
 	if actual != expected {
 		t.Errorf("actual:%v, expected:%v", actual, expected)
